f11d53f5135b0569d1307667c298ffc08114cf51
Rainymaw <saidanis.khouider@outlook.fr>
2024-09-05 10:10:55 +0100

    adding project to github

---
+++ index.js
@@ -0,0 +1,40 @@
+//Exercice 1
+VAT calculationWrite a program that asks the user for a raw price. After that, it calculates the correspondingfinal price using a VAT rate of 20.6%.
+//Exercice 2
+From Celsius to Fahrenheit degreesWrite a program that asks for a temperature in Celsius degrees, then displays it in Fahrenheitdegrees.The conversion between scales is given by the formula: [°F] = [°C] x 9/5 + 32.
+//Exercice 3
+let number1=5;let number2=3;
+// TODO: type your code here (and nowhere else!)
+console.log(number1);// Should show 3
+console.log(number2);// Should show 5
+Add the necessary code to swap between number1 and number2
+//Exercice 4
+Following dayWrite a program that accepts a day name from the user, then shows the name of the followingday. Incorrect inputs must be taken into account
+//Exercice 5
+Number comparisonWrite a program that accepts two numbers, then compares their values and displays anappropriate message in all cases
+//Exercice 6
+Number of days in a monthWrite a program that accepts a month number (between 1 and 12), then shows the number ofdays of that month. Leap years are excluded. Incorrect inputs must be taken into account.
+//exercice 7
+Multiplication tableWrite a program that asks the user for a number, then shows the multiplication table for thisnumber.When you are done, improve the program so it only accepts numbers between 2 and 9 (use theprevious exercise as a blueprint).
+//Exercice 8
+FizzBuzzWrite a program that shows all numbers between 1 and 100 with the following exceptions:•It shows"Fizz"instead if the number is divisible by 3.•It shows"Buzz"instead if the number is divisible by 5 and not by 3.When it’s done, improve it so that the program shows"FizzBuzz"instead for numbers divisibleboth by 3 and by 5.
+//Exercice 9Z
+Circumference and area of a circleWrite a program containing two functions to calculate the circumference and area of a circledefined by its radius. Test it using user input.Here are some tips for solving this exercise:•Circumference and area calculation formulas should be part of your secondary schoolmemories... Or a Google click away :)•The value of number π (Pi) is obtained withMath.PIin JavaScript.•You might want to use theexponentiation operator²**to perform computations.
+//Exercice 10
+Write a program that creates anaccountobject with the following characteristics:•Anameproperty set to “Alex”.•Abalanceproperty set to 0.•Acreditmethod adding the (positive or negative) value passed as an argument to theaccount balance.•Adescribemethod returning the account description.Use this object to show its description, crediting 250, debiting 80, then show its description again
+//Exercice 11
+Write a program that creates the following array, then calculates and shows the sum of its values(42 in that case).constvalues=[3,11,7,2,9,10];
+//Exercice 12
+Write a program that creates the following array, then calculates and shows the array’s maximumvalue.constvalues=[3,11,7,2,9,10];
+//Exercice 13
+Word infoWrite a program that asks you for a word then shows its length, lowercase, and uppercase values.Vowel countImprove the previous program so that it also shows the number of vowels inside the word.Backwards wordImprove the previous program so that it shows the word written backwards.www.dbooks.org
+Work with strings72PalindromeImprove the previous program to check if the word is a palindrome. A palindrome is a word orsentence that’s spelled the same way both forward and backward, ignoring punctuation, case,and spacing
+//Exercice 14
+Older moviesImprove the example movie program from above so that it shows the titles of movies releasedbefore year 2000, using functional programming.constmovieList=[{title:"Batman",year:1989,director:"Tim Burton",imdbRating:7.6},{title:"Batman Returns",year:1992,director:"Tim Burton",imdbRating:7.0},{title:"Batman Forever",¹https://www.w3.org/community/webed/wiki/A_Short_History_of_JavaScript²https://en.wikipedia.org/wiki/Scheme_(programming_language)
+    Discover functional programming97year:1995,director:"Joel Schumacher",imdbRating:5.4},{title:"Batman & Robin",year:1997,director:"Joel Schumacher",imdbRating:3.7},{title:"Batman Begins",year:2005,director:"Christopher Nolan",imdbRating:8.3},{title:"The Dark Knight",year:2008,director:"Christopher Nolan",imdbRating:9.0},{title:"The Dark Knight Rises",year:2012,director:"Christopher Nolan",imdbRating:8.5}];
+//Exercice 15
+//Exercice 16
+//Exercice 17
+//Exercice 18
+//Exercice 19
+//Exercice 20
